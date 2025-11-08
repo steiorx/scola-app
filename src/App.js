649107aa7f0f1c5971563c106c9927aa0d7d287d
steiorx <stefaniordan2008@gmail.com>
2025-11-08 17:34:58 +0200

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import NumberTable from './components/NumberTable';
 import './App.css';
 
+let amount = Array(11).fill(0);
+
 function App() {
+  const [av, setAv] = useState(0);
+
+  function modifyAverage(number, newAmount) {
+    console.log(number, newAmount);
+    amount[number] = newAmount;
+    let tsum = 0, tgrades = 0;
+
+    for (let i = 1; i <= 10; i++) {
+      tsum += amount[i] * i;
+      tgrades += amount[i];
+    }
+
+    let average = tgrades === 0 ? 0 : tsum / tgrades;
+    if (average === 10) setAv(10);
+    else setAv(average.toPrecision(3));
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NumberTable modifyAverage={modifyAverage}/>
+      <p className='averageDisplay'>The average is {av}</p>
     </div>
   );
 }
